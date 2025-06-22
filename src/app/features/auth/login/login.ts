9ddbe4827f1e4073fb10253326e8d128acb8b86a
Adrian <adrian13bracamonte@gmail.com>
2025-06-22 20:59:44 -0300

    agregue las validaciones del login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,29 +1,37 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from "@angular/forms";
-import { Router } from "@angular/router";
-
+import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
 
   loginForm: FormGroup;
+  submitted = false;
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
-      email: ["", [Validators.required, Validators.email]],
+      cuil: ["", [Validators.required, Validators.pattern(/^\d{11}$/)]],
       password: ["", [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  onSubmit() {
-    if (this.loginForm.invalid) return;
+  get f() {
+    return this.loginForm.controls;
+  }
 
-    const { email, password } = this.loginForm.value;
+  onSubmit() {
+    const { cuil, password } = this.loginForm.value;
+
+    if (cuil === "20123456789" && password === "123456") {
+      this.router.navigate(["/student"]);
+    }
 
   }
 }
