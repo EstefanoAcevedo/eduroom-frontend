8d473bf84f68d2b4dd7e2a274c0e6ebb533a86f8
Adrian <adrian13bracamonte@gmail.com>
2025-07-03 18:46:25 -0300

    desing

--- src/app/features/student/student-dashboard/student-dashboard.ts
+++ src/app/features/student/student-dashboard/student-dashboard.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-dashboard',
-  imports: [],
+  standalone: true, // clave en Angular 20
+  imports: [CommonModule], // necesario para *ngFor, *ngIf, etc.
   templateUrl: './student-dashboard.html',
-  styleUrl: './student-dashboard.css'
+  styleUrls: ['./student-dashboard.css']
 })
-export class StudentDashboard {
+export class StudentDashboard implements OnInit {
+
+  estudiante: any;
+  ultimaAsistencia: any;
+  materiasActivas: any[] = [];
+  notificaciones: any[] = [];
+
+  ngOnInit(): void {
+    this.estudiante = {
+      nombre: 'Adrián Bracamonte'
+    };
+
+    this.ultimaAsistencia = {
+      fecha: '2025-06-28',
+      materia: 'Matemática'
+    };
+
+    this.materiasActivas = [
+      { nombre: 'Matemática' },
+      { nombre: 'Lengua' },
+      { nombre: 'Programación' }
+    ];
+
+    this.notificaciones = [
+      { id: 1, mensaje: 'Falta registrada el 25/06' },
+      { id: 2, mensaje: 'Nueva clase de refuerzo disponible' }
+    ];
+  }
 
 }
--- src/app/features/student/student.routes.ts
+++ src/app/features/student/student.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
+
 import { StudentAttendanceHistory } from './student-attendance-history/student-attendance-history';
 import { StudentDashboard } from './student-dashboard/student-dashboard';
 import { StudentSubjects } from './student-subjects/student-subjects';
 
 export const student_routes: Routes = [
-    { path: 'attendance-history', component: StudentAttendanceHistory },
-    { path: 'dashboard', component: StudentDashboard },
-    { path: 'subjects', component: StudentSubjects },
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+    {
+        path: '',
+
+        children: [
+            { path: 'attendance-history', component: StudentAttendanceHistory },
+            { path: 'dashboard', component: StudentDashboard },
+            { path: 'subjects', component: StudentSubjects },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+        ]
+    }
 ];
