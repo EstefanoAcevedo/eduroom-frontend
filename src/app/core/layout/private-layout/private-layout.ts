b95d7c9cc4c3a909c2227b2beadda211c2ef411c
Estéfano Marcial Acevedo <acevedoestefano4@gmail.com>
2025-07-03 18:47:45 -0300

    WIP(Sidebar)

--- src/app/core/layout/private-layout/private-layout.ts
+++ src/app/core/layout/private-layout/private-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "../../../shared/components/header/header";
 import { Footer } from "../../../shared/components/footer/footer";
+import { Sidebar } from "../../../shared/components/sidebar/sidebar";
 
 @Component({
   selector: 'app-private-layout',
-  imports: [Header, Footer, RouterOutlet],
+  imports: [Header, Footer, RouterOutlet, Sidebar],
   templateUrl: './private-layout.html',
   styleUrl: './private-layout.css'
 })
