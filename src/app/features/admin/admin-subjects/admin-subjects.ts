2ea1ccebf94ccb038eb33883d7cc9ed5fcdde368
Estéfano Marcial Acevedo <acevedoestefano4@gmail.com>
2025-07-27 20:19:17 -0300

    feat(teacher-subjects, admin-subjects)

---
+++ src/app/features/admin/admin-subjects/admin-subjects.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Subjects } from "../../subjects/subjects/subjects";
+
+@Component({
+  selector: 'app-admin-subjects',
+  imports: [Subjects],
+  templateUrl: './admin-subjects.html',
+  styleUrl: './admin-subjects.css'
+})
+export class AdminSubjects {
+
+}
